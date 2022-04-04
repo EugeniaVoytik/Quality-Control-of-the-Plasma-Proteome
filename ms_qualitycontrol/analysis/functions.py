e17bef0d0119d4b8db98879c357a81c07b5f77e7
EugeniaVoytik <jane.voytik@gmail.com>
2022-04-04 19:07:45 +0200

    REFACTOR: update the file according to pep

--- ms_qualitycontrol/analysis/functions.py
+++ ms_qualitycontrol/analysis/functions.py
@@ -6,8 +6,10 @@ import re
 import itertools
 import statistics
 
+
 def select_cols(columns):
-    return 'LFQ' in columns or columns in ['Gene names', 'Protein IDs', 'Protein names']
+    return 'LFQ' in columns or columns in \
+        ['Gene names', 'Protein IDs', 'Protein names']
 
 
 def read_own_table(filepath):
@@ -30,33 +32,50 @@ def lower_input(user_input):
 
 def get_list_of_col(df, searched_value):
     if len(searched_value) == 1:
-        columns = [column for column in df.columns if searched_value[0] in column.lower()]
+        columns = [
+            column for column in df.columns if searched_value[0]
+            in column.lower()
+        ]
         return columns
     elif len(searched_value) > 1:
         columns = []
         for each in searched_value:
-            columns.append([column for column in df.columns if each in column.lower()])
+            columns.append([
+                column for column in df.columns if each in column.lower()
+            ])
         return list(itertools.chain.from_iterable(columns))
 
 
 def get_reversed_list_of_col(df, searched_value):
     if len(searched_value) == 1:
-        columns = [column for column in df.columns if searched_value[0] not in column.lower()]
+        columns = [
+            column for column in df.columns if searched_value[0]
+            not in column.lower()
+        ]
         return columns
     elif len(searched_value) > 1:
         columns = []
         for each in searched_value:
-            columns.append([column for column in df.columns if each not in column.lower()])
+            columns.append([
+                column for column in df.columns if each not in column.lower()
+            ])
         return list(itertools.chain.from_iterable(columns))
 
+
 def two_sample_t_test(control, samples, df):
-    t_stat, p_val = stats.ttest_ind(control, samples, axis=1, nan_policy='omit')
+    t_stat, p_val = stats.ttest_ind(
+        control,
+        samples,
+        axis=1,
+        nan_policy='omit'
+    )
     df['p_val'], df['(-)log10_p_val'] = p_val, -np.log10(p_val)
     return df
 
 
 def calculate_mean(control, samples, df):
-    df['control_mean'], df['samples_mean'] = np.mean(control, axis=1), np.mean(samples, axis=1)
+    df['control_mean'], df['samples_mean'] = np.mean(control, axis=1), \
+        np.mean(samples, axis=1)
     return df
 
 
@@ -72,13 +91,17 @@ def select_LFQ_cols(columns):
 
 def filter_valid_values(df, index, numeric_columns, percent, ax):
     df_copied = df.set_index(index)[numeric_columns]
-    new_df = df_copied.dropna(thresh=int(len(df_copied.columns) * percent), axis=ax)
+    new_df = df_copied.dropna(
+        thresh=int(len(df_copied.columns) * percent),
+        axis=ax
+    )
     return new_df.reset_index()
 
 
 def get_list_of_indices(df, column, mask, separator):
     """
-    A function that can be used for sorting of Data Frame based on a created list of values(mask).
+    A function that can be used for sorting of Data Frame based
+    on a created list of values(mask).
     Args:
         df:  A Data Frame which we'd like to sort based on a mask.
         mask: A column based on which we'd like to sort.
@@ -107,26 +130,54 @@ def extract_val_from_col(df, column, separator):
     Returns:A list of all values of a column.
 
     """
-    values_protIDs = [value.split(separator) for value in df[column].dropna(axis=0)]
+    values_protIDs = [
+        value.split(separator) for value in df[column].dropna(axis=0)
+    ]
     return list(itertools.chain.from_iterable(values_protIDs))
 
 
 def normal_ratio(df, marker, num_std, reverse=False):
-    if reverse==False:
-        test = np.array(np.sum(marker, axis=0) / np.sum(df, axis=0), dtype=float).round(decimals=4)
-        std_bn_samples = (statistics.harmonic_mean(test) + np.std(test) * num_std).round(decimals=4)
+    if reverse is False:
+        test = np.array(
+            np.sum(marker, axis=0) / np.sum(df, axis=0),
+            dtype=float
+        ).round(decimals=4)
+        std_bn_samples = (
+            statistics.harmonic_mean(test) + np.std(test) * num_std
+            ).round(decimals=4)
         return test, std_bn_samples
     else:
-        test = 1/np.array(np.sum(marker, axis=0) / np.sum(df, axis=0), dtype=float).round(decimals=4)
-        std_bn_samples = (statistics.harmonic_mean(test) + np.std(test) * num_std).round(decimals=4)
+        test = 1/np.array(
+            np.sum(marker, axis=0) / np.sum(df, axis=0), dtype=float
+            ).round(decimals=4)
+        std_bn_samples = (
+            statistics.harmonic_mean(test) + np.std(test) * num_std
+            ).round(decimals=4)
         return test, std_bn_samples
 
+
 def make_annotation_item(x, y, z, color):
-    return {'xref': 'x', 'yref': 'y', 'x': x, 'y': y, 'text': '#%s' % z.replace('LFQ', '').replace(' intensity', ''),
-            'showarrow': True, 'font': dict(family='Arial', size=12, color='white'),
-            'align': 'left', 'arrowhead': 2, 'arrowsize': 1, 'arrowwidth': 2, 'arrowcolor': '#A9A9A9',
-            'ax': 20, 'ay': -30, 'bordercolor': '#A9A9A9', 'borderwidth': 2, 'borderpad': 4, 'bgcolor': color,
-            'opacity': 0.8}
+    return {
+        'xref': 'x',
+        'yref': 'y',
+        'x': x,
+        'y': y,
+        'text': '#%s' % z.replace('LFQ', '').replace(' intensity', ''),
+        'showarrow': True,
+        'font': dict(family='Arial', size=12, color='white'),
+        'align': 'left',
+        'arrowhead': 2,
+        'arrowsize': 1,
+        'arrowwidth': 2,
+        'arrowcolor': '#A9A9A9',
+        'ax': 20,
+        'ay': -30,
+        'bordercolor': '#A9A9A9',
+        'borderwidth': 2,
+        'borderpad': 4,
+        'bgcolor': color,
+        'opacity': 0.8
+    }
 
 
 def create_annotations(data, threshold, col_names):
@@ -137,7 +188,11 @@ def create_annotations(data, threshold, col_names):
             data_above_threshold_indices.append(i)
     names = [col_names[i] for i in data_above_threshold_indices]
     annotations = []
-    for x, y, z in zip(data_above_threshold_indices, data_above_threshold, names):
+    for x, y, z in zip(
+        data_above_threshold_indices,
+        data_above_threshold,
+        names
+    ):
         annotations.append(make_annotation_item(x, y, z, '#F19F4D'))
     return annotations
 
